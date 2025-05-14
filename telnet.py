e9ffbe5f773bb03e79649f81ef4a1c4e3feb7e9d
Sushanthbhat23 <sushanthbhat23@gmail.com>
2025-05-14 17:59:27 +0530

    Update telnet.py

--- telnet.py
+++ telnet.py
@@ -1,6 +1,7 @@
 import socket
 
 class URL:
+    #parses through the url 
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
         assert self.scheme == "http"
@@ -9,7 +10,8 @@ class URL:
             url = url + "/"
             self.host, url = url.split("/", 1)
             self.path = "/" + url
-        
+
+    #sets up connection between 2 computers  
     def request(self):
         s = socket.socket(
             family = socket.AF_INET,
@@ -17,3 +19,12 @@ class URL:
             proto = socket.IPPROTO_TCP,
         )
         s.connect((self.host, 80))
+
+        #sets upe request to the other server
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+
+    
+        
