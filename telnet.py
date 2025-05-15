ba6d3aabd48b2a370ddec57399daaa10493f2435
Sushanthbhat23 <sushanthbhat23@gmail.com>
2025-05-15 12:58:13 +0530

    complete telnet.py

--- telnet.py
+++ telnet.py
@@ -1,37 +1,34 @@
 import socket
 
 class URL:
-    #parses through the url 
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
         assert self.scheme == "http"
 
-        if "/" not in url:
-            url = url + "/"
+        if "/" in url:
             self.host, url = url.split("/", 1)
             self.path = "/" + url
+        else:
+            self.host = url
+            self.path = "/"
 
-    #sets up connection between 2 computers  
     def request(self):
         s = socket.socket(
-            family = socket.AF_INET,
-            type = socket.SOCK_STREAM,
-            proto = socket.IPPROTO_TCP,
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
         )
         s.connect((self.host, 80))
 
-        #sets upe request to the other server
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
         request += "\r\n"
         s.send(request.encode("utf8"))
-        response = s.makefile("r", encoding = "utf8", newline = "\r\n") #used to read the servers response
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
 
-        #splitting response into pieces and recording statusline
         statusline = response.readline()
         version, status, explanation = statusline.split(" ", 2)
 
-        #recording header
         response_headers = {}
         while True:
             line = response.readline()
@@ -39,14 +36,12 @@ class URL:
             header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
 
-            #making sure that certain headers that tell us data is sent in an unusual way is not present
-            assert "transfer-encoding" not in response_headers
-            assert "content-encoding" not in response_headers
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
 
-            #reading other content
-            content = response.read()
-            s.close()
-            return content #returns the content that has been read in this function
+        content = response.read()
+        s.close()
+        return content
         
     def show(self, body):
         in_tag = False
@@ -54,17 +49,17 @@ class URL:
             if c == "<":
                 in_tag = True
             elif c == ">":
-                in_tag = True
+                in_tag = False
             elif not in_tag:
-                print(c, end = " ")
+                print(c, end="")
     
-    #loading the url
-    def load(self, url):
-        body = URL(url).request()
+    def load(self):  # Removed the `url` parameter
+        body = self.request()  # Uses `self` (the URL object)
         self.show(body)
 
-        if __name__ == "__main__":
-            import sys
-            self.load(URL(sys.argv[1]))
+if __name__ == "__main__":
+    import sys
+    url = URL(sys.argv[1])  # Parse the URL from command line
+    url.load()  # Now correctly calls `load()` without arguments
 
         
